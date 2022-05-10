f754f460525e72bc9452985dc6784550eb2edaa5
bopboy <39391891+bopboy@users.noreply.github.com>
2022-05-11 07:22:54 +0900

    08 리팩토링

--- src/index.js
+++ src/index.js
@@ -5,16 +5,24 @@ const sub = document.getElementById('sub')
 const number = document.querySelector('span')
 
 number.innerText = 0
+const ADD = "ADD"
+const SUB = "SUB"
 
 const countModifier = (count = 0, action) => {
   console.log(count, action)
-  if (action.type === 'ADD') return count += 1
-  else if (action.type === 'SUB') return count -= 1
-  else return count
+  switch (action.type) {
+    case ADD: return count + 1
+    case SUB: return count - 1
+    default: return count
+  }
 }
 const countStore = createStore(countModifier)
+
 const onChange = () => { number.innerText = countStore.getState() }
 countStore.subscribe(onChange)
 
-add.addEventListener('click', () => countStore.dispatch({ type: 'ADD' }))
-sub.addEventListener('click', () => countStore.dispatch({ type: 'SUB' }))
+const handleAdd = () => { countStore.dispatch({ type: ADD }) }
+const handleSub = () => { countStore.dispatch({ type: SUB }) }
+
+add.addEventListener('click', handleAdd)
+sub.addEventListener('click', handleSub)
