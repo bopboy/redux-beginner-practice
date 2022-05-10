2260ed39a37148ba3503759c3f2d8cfd266669de
bopboy <39391891+bopboy@users.noreply.github.com>
2022-05-11 07:18:35 +0900

    07 이벤트 핸들러 연결, 스토어 구독

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const add = document.getElementById('add')
 const sub = document.getElementById('sub')
 const number = document.querySelector('span')
 
+number.innerText = 0
+
 const countModifier = (count = 0, action) => {
   console.log(count, action)
   if (action.type === 'ADD') return count += 1
@@ -11,17 +13,8 @@ const countModifier = (count = 0, action) => {
   else return count
 }
 const countStore = createStore(countModifier)
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "SUB" })
-countStore.dispatch({ type: "SUB" })
-countStore.dispatch({ type: "SUB" })
-countStore.dispatch({ type: "SUB" })
-countStore.dispatch({ type: "SUB" })
-countStore.dispatch({ type: "SUB" })
-countStore.dispatch({ type: "SUB" })
-console.log(countStore.getState())
+const onChange = () => { number.innerText = countStore.getState() }
+countStore.subscribe(onChange)
+
+add.addEventListener('click', () => countStore.dispatch({ type: 'ADD' }))
+sub.addEventListener('click', () => countStore.dispatch({ type: 'SUB' }))
