856928c58dd1bb6031c1c8d5dc472459b5094905
bopboy <39391891+bopboy@users.noreply.github.com>
2022-05-11 07:08:46 +0900

    06 액션, 디스패치

--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ const add = document.getElementById('add')
 const sub = document.getElementById('sub')
 const number = document.querySelector('span')
 
-const countModifier = (state = 0) => {
-  console.log(state)
-  return state
+const countModifier = (count = 0, action) => {
+  console.log(count, action)
+  if (action.type === 'ADD') return count += 1
+  else if (action.type === 'SUB') return count -= 1
+  else return count
 }
 const countStore = createStore(countModifier)
-console.log(countStore)
+countStore.dispatch({ type: "ADD" })
+countStore.dispatch({ type: "ADD" })
+countStore.dispatch({ type: "ADD" })
+countStore.dispatch({ type: "ADD" })
+countStore.dispatch({ type: "ADD" })
+countStore.dispatch({ type: "ADD" })
+countStore.dispatch({ type: "SUB" })
+countStore.dispatch({ type: "SUB" })
+countStore.dispatch({ type: "SUB" })
+countStore.dispatch({ type: "SUB" })
+countStore.dispatch({ type: "SUB" })
+countStore.dispatch({ type: "SUB" })
+countStore.dispatch({ type: "SUB" })
 console.log(countStore.getState())
