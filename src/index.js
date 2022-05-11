9c1ab24a605e2eb1cee6b13f6310f1e0887cc525
bopboy <39391891+bopboy@users.noreply.github.com>
2022-05-11 21:59:20 +0900

    09-2 리덕스 도입

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+import { createStore } from 'redux'
+
+
 const form = document.querySelector('form')
 const input = document.querySelector('input')
 const ul = document.querySelector('ul')
+
+const ADD_TODO = "ADD_TODO"
+const DELETE_TODO = "DELETE_TODO"
+
+const reducer = (state = [], action) => {
+  console.log(action)
+  switch (action.type) {
+    case ADD_TODO: return []
+    case DELETE_TODO: return []
+    default: return state
+  }
+}
+const store = createStore(reducer)
 
 const toDos = []
 
@@ -13,6 +29,7 @@ const onSubmit = e => {
   e.preventDefault()
   const toDo = input.value
   input.value = ''
-  createTodo(toDo)
+  // createTodo(toDo)
+  store.dispatch({ type: ADD_TODO, text: toDo })
 }
 form.addEventListener('submit', onSubmit)
