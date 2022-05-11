80d11f20eb546c381393ae1e1ffc9e22de67364a
bopboy <39391891+bopboy@users.noreply.github.com>
2022-05-12 05:46:53 +0900

    11-1 투두 생성

--- src/index.js
+++ src/index.js
@@ -7,29 +7,37 @@ const ul = document.querySelector('ul')
 
 const ADD_TODO = "ADD_TODO"
 const DELETE_TODO = "DELETE_TODO"
+const toDos = []
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD_TODO: return [...state, { text: action.text, id: Date.now() }]
+    case ADD_TODO: return [{ text: action.text, id: Date.now() }, ...state]
     case DELETE_TODO: return []
     default: return state
   }
 }
 const store = createStore(reducer)
 
-const toDos = []
+const paintToDos = () => {
+  const toDos = store.getState()
+  ul.innerHTML = ''
+  toDos.forEach(toDo => {
+    const li = document.createElement('li')
+    li.id = toDo.id
+    li.innerText = toDo.text
+    ul.append(li)
+  })
+}
+store.subscribe(paintToDos)
 
-const createTodo = toDo => {
-  const li = document.createElement('li')
-  li.innerText = toDo
-  ul.append(li)
+const addToDo = text => {
+  store.dispatch({ type: ADD_TODO, text })
 }
 const onSubmit = e => {
   e.preventDefault()
   const toDo = input.value
   input.value = ''
-  store.dispatch({ type: ADD_TODO, text: toDo })
+  input.focus()
+  addToDo(toDo)
 }
 form.addEventListener('submit', onSubmit)
-
-store.subscribe(() => console.log(store.getState()))
