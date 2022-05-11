2813c9105852d46a0a629198583cd706a71e6719
bopboy <39391891+bopboy@users.noreply.github.com>
2022-05-12 06:41:52 +0900

    14 리액트 프로젝트로 변경
    리액트 버전 17, 리액트 라우터 돔 5

--- src/index.js
+++ src/index.js
@@ -1,55 +1,5 @@
-import { createStore } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
 
-
-const form = document.querySelector('form')
-const input = document.querySelector('input')
-const ul = document.querySelector('ul')
-
-const ADD_TODO = "ADD_TODO"
-const DELETE_TODO = "DELETE_TODO"
-const toDos = []
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO: return [{ text: action.text, id: Date.now() }, ...state]
-    case DELETE_TODO: return state.filter(toDo => toDo.id !== action.id)
-    default: return state
-  }
-}
-const store = createStore(reducer)
-
-const paintToDos = () => {
-  const toDos = store.getState()
-  ul.innerHTML = ''
-  toDos.forEach(toDo => {
-    const li = document.createElement('li')
-    const btn = document.createElement('button')
-    btn.innerText = 'Del'
-    btn.addEventListener('click', dispatchDeleteToDo)
-    li.id = toDo.id
-    li.innerText = toDo.text
-    li.append(btn)
-    ul.append(li)
-  })
-}
-
-store.subscribe(paintToDos)
-
-const addToDo = text => { return { type: ADD_TODO, text } }
-const deleteToDo = id => { return { type: DELETE_TODO, id } }
-
-const dispatchAddToDo = text => {
-  store.dispatch(addToDo(text))
-}
-const dispatchDeleteToDo = (e) => {
-  const id = parseInt(e.target.parentNode.id)
-  store.dispatch(deleteToDo(id))
-}
-const onSubmit = e => {
-  e.preventDefault()
-  const toDo = input.value
-  input.value = ''
-  input.focus()
-  dispatchAddToDo(toDo)
-}
-form.addEventListener('submit', onSubmit)
+ReactDOM.render(<App />, document.getElementById('root'))
